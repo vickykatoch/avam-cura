21d00a394a615d04de3bd086589c5b3724d55a7a
Balwinder Katoch <vickykatoch@yahoo.com>
2016-08-23 20:22:41 -0400

    changes

--- server/server.prd.js
+++ server/server.prd.js
@@ -5,7 +5,7 @@ import express from 'express';
 let app = express();
 let staticPath = path.join(__dirname,'../www/assets');
 
-app.use('/wwww/assets', express.static(staticPath));
+app.use('/www/assets', express.static(staticPath));
 
 app.get('/', (req, res) => {    
   res.sendFile(path.join(__dirname, '../www/index.html'));
--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello React 3</h1>
+                <h1>Hello React 323</h1>
             </div>
         );
     }
