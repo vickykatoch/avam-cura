0416541cbc4b4e91dd3f95e29cfa840699b4b755
Balwinder Katoch <Balwinder Katoch>
2016-09-01 08:01:41 -0400

    ripple added

--- src/common/components/ComponentIdentifiers.js
+++ src/common/components/ComponentIdentifiers.js
@@ -1,3 +1,6 @@
-export const APP_BAR = 'AVAppBar';
-
-export const PROGRESS_BAR = 'RTProgressBar';
+export const APP_BAR                    =       'AVAppBar';
+export const PROGRESS_BAR               =       'AVProgressBar';
+export const DRAWER                     =       'AVDrawer';
+export const OVERLAY                     =       'AVOverlay';
+export const BUTTON                     =       'AVButton';
+export const RIPPLE                     =       'AVRipple';
