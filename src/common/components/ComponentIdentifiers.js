9ed32902b7f0b2b3a9853e28091a7befb14bb35f
Balwinder Katoch <Balwinder Katoch>
2016-09-01 08:13:21 -0400

    Checkbox added

--- src/common/components/ComponentIdentifiers.js
+++ src/common/components/ComponentIdentifiers.js
@@ -1,6 +1,7 @@
 export const APP_BAR                    =       'AVAppBar';
 export const PROGRESS_BAR               =       'AVProgressBar';
 export const DRAWER                     =       'AVDrawer';
-export const OVERLAY                     =       'AVOverlay';
+export const OVERLAY                    =       'AVOverlay';
 export const BUTTON                     =       'AVButton';
 export const RIPPLE                     =       'AVRipple';
+export const CHECKBOX                   =       'AVCheckBox';
